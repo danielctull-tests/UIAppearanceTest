4eeefb77100f8365cee2f6b14c8c906dca7b7f2d
Daniel Tull <dt@danieltull.co.uk>
2012-01-26 09:30:20 +0000

    Initial Commit

---
+++ UIAppearanceTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIAppearanceTest
+//
+//  Created by Daniel Tull on 26.01.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
